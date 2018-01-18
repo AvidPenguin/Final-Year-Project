711986fafdc517ea0203fecf4f9b233e607bddd3
unknown <avidpenguin@gmail.com>
2018-01-18 00:55:31 +0000

    ASP.NET MVC Init

---
+++ CoasterCredits/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoasterCredits.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
